4b880ea259f7e3ca47b0432f869e194280e9a94c
Selimmyrat <gandymovspc@gmail.com>
2025-11-25 16:13:38 +0500

    added header logo, menu, sidebarmenu in progress

---
+++ components/Signin.tsx
@@ -0,0 +1,9 @@
+const Signin = () => {
+  return (
+    <button className="text-sm font-semibold hover:darkColor text-lightColor hover:cursor-pointer hoverEffect">
+      Login
+    </button>
+  );
+};
+
+export default Signin;
