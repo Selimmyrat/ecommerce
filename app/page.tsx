54472e00e164d590cd5fd23d4ae2a1c9f3249f33
Selimmyrat <gandymovspc@gmail.com>
2025-11-26 10:56:46 +0500

    added home banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,10 @@
 import Container from "@/components/Container";
-import { Button } from "@/components/ui/button";
+import HomeBanner from "@/components/HomeBanner";
 
 const Home = () => {
   return (
     <Container className="bg-shop-light-pink">
-      <h2 className="text-2xl font-bold">Home</h2>
-      <p className="text-gray-600">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad
-        reprehenderit illo delectus, deserunt odit nesciunt! Excepturi
-        dignissimos natus sapiente culpa? Ratione veritatis nam corporis rerum
-        suscipit aut nostrum nobis ipsam?
-      </p>
-      <Button>Check out</Button>
+      <HomeBanner />
     </Container>
   );
 };
